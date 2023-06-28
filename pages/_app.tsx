1119e5fec24035b2a6e0d9fa84bede1a742c5125
Matias Guerrero <matias.f.guerrero19@gmail.com>
2023-06-28 02:21:33 -0300

    feat inicio seccion 7

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const basicTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+  });
+
+function App({ Component, pageProps }: AppProps) {  
+  return
+  (
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  ) 
 }
+
+export default App;
